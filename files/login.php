ac9d595d3ef97422036f782fc44bb9e5c1250cdc
rbsl-yasmin <yasmin@readybytes.in>
2016-09-15 17:27:00 +0530

    Update and rename map.php to login.php

--- files/login.php
+++ files/login.php
@@ -1,65 +1,21 @@
 <?php
 // no direct access
 defined('_JEXEC') or die;
-
-//$params->def('greeting', 1);
-
-$user = JFactory::getUser();
-$type	= (!$user->get('guest')) ? 'logout' : 'login';
-//$items 	= 	JFactory::getApplication()->getPathway()->getPathWay();
-//$item	=	array_pop($items);
-$link	= JURI::getInstance()->toString();
-//if(isset($item->link)) {
-//	$link = $item->link;
-//}
-$return	= 	base64_encode($link);
-
-//JHtml::_('behavior.keepalive');
 ?>
-<?php if ($type == 'logout') : ?>
-<form action="<?php echo JRoute::_('index.php'); ?>" method="post" id="login-form">
-	<div class="logout-button">
-		<input type="submit" name="Submit" class="button" value="<?php echo JText::_('JLOGOUT'); ?>" />
-		<input type="hidden" name="option" value="com_users" />
-		<input type="hidden" name="task" value="user.logout" />
-		<input type="hidden" name="return" value="<?php echo $return; ?>" />
-		<?php echo JHtml::_('form.token'); ?>
-	</div>
-</form>
-<?php else : ?>
-	<div class="conatiner-fluid">
-		<form action="<?php echo JRoute::_('index.php'); ?>" method="post" id="login-form" class="custom-popup-login">
-                    <fieldset class="userdata row-fluid">
-                        <div class="input-prepend">
-                            <span class="add-on"><i class="icon-user"></i></span>
-                            <input type="text" name="username"  placeholder="Username or Email"/>
-                        </div>
-                        <div class="input-prepend">
-                            <span class="add-on"><i class="icon-lock"></i></span>
-                            <input type="password" name="password" placeholder="Password"/>
-                        </div>
-                            <?php if (JPluginHelper::isEnabled('system', 'remember')) : ?>
-                        <div class="row-fluid" style="margin-top: 20px;">
-                            <div class="span6" style="padding: 10px 0; margin-right: 10px;">
-                                <label class="checkbox">
-                                    <input id="modlgn-remember" type="checkbox" name="remember" class="inputbox" value="yes"/>
-                                    Remember me
-                                </label>
-                            </div>
-                            <div class="span4">
-                                <?php endif; ?>
-                                <input type="submit" name="Submit" class="btn btn-primary btn-block" value="Login" />
-                                <input type="hidden" name="option" value="com_users" />
-                                <input type="hidden" name="task" value="user.login" />
-                                <input type="hidden" name="return" value="<?php echo $return; ?>" />
-                                    <?php echo JHtml::_('form.token'); ?>
-                            </div>
-                        </div>
-                    </fieldset>
-</form>
-        </div>
-        <div style="margin: 40px 35px 0 0" class="text-center" >
-            <a style="color: #888;" href="<?php echo JRoute::_('index.php?option=com_users&view=reset'); ?>">Forgot Your Password ?</a>
-        </div>
-
-<?php endif; ?>
+<script type="text/javascript">
+    google.maps.event.addDomListener(window, 'load', init);
+     function init() {
+          var mapOptions = {
+              zoom: 11,
+               scrollwheel: false,
+               navigationControl: false,
+               mapTypeControl: false,
+               scaleControl: false,
+              draggable: true,
+               center: new google.maps.LatLng(59.73798650000001, 10.2034322), 
+         }
+         var mapElement = document.getElementById('ellingsen');
+     }
+</script>
+<p style="[{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+               };margin-bottom: 0; height: 411px;"><iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2053.7413537962284!2d8.519048251759624!3d59.019549142773094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464762c882a7bec9%3A0x4ea20da3feaada72!2sFredriksens+Fabrikk+AS!5e0!3m2!1sen!2sus!4v1473852589262" width="100%" height="100%" frameborder="0" allowfullscreen=""></iframe></p>
