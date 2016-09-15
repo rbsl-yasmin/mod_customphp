58d73cd7850d69556a7f8e071d0beba17c70f61b
rbsl-yasmin <yasmin@readybytes.in>
2016-09-15 17:20:17 +0530

    Update and rename login.php to map.php

---
+++ files/map.php
@@ -0,0 +1,21 @@
+<?php
+// no direct access
+defined('_JEXEC') or die;
+?>
+<script type="text/javascript">
+    google.maps.event.addDomListener(window, 'load', init);
+     function init() {
+          var mapOptions = {
+              zoom: 11,
+               scrollwheel: false,
+               navigationControl: false,
+               mapTypeControl: false,
+               scaleControl: false,
+              draggable: true,
+               center: new google.maps.LatLng(59.73798650000001, 10.2034322), 
+         }
+         var mapElement = document.getElementById('ellingsen');
+     }
+</script>
+<p style="[{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+               };margin-bottom: 0; height: 411px;"><iframe style="border: 0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2053.7413537962284!2d8.519048251759624!3d59.019549142773094!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464762c882a7bec9%3A0x4ea20da3feaada72!2sFredriksens+Fabrikk+AS!5e0!3m2!1sen!2sus!4v1473852589262" width="100%" height="100%" frameborder="0" allowfullscreen=""></iframe></p>
